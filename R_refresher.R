ea6232919fd4436edf4a539ecf32086f4e0c187c
adela-pafkova <adela.pafkova@gmail.com>
2023-10-01 21:43:06 +0100

    Finished excercise 2

--- R_refresher.R
+++ R_refresher.R
@@ -28,5 +28,38 @@ abline(v=median(soils$Soil_pH))
 abline(v=mean(soils$Soil_pH)) 
 
 ## plot diff relationships
-plot(Soil_pH~Habitat,data=soils) 
-plot(Potassium~Habitat,data=soils) 
+boxplot(Soil_pH~Habitat,data=soils) 
+boxplot(Potassium~Habitat,data=soils) 
+
+## anovas
+lm_pH <- lm(Soil_pH~Habitat,data=soils) 
+anova(lm_pH) 
+lm_K <- lm(Potassium~Habitat,data=soils) 
+anova(lm_K) 
+
+## check assumptions
+lm_pH_resids <- resid(lm_pH) 
+shapiro.test(lm_pH_resids) 
+bartlett.test(Soil_pH ~Habitat,data=soils) 
+lm_K_resids <- resid(lm_K) 
+shapiro.test(lm_K_resids) 
+bartlett.test(Potassium ~Habitat,data=soils) 
+
+plot(lm_pH) 
+plot(lm_K) 
+
+### boxplots to examine why assumptions are not met
+hist(soils$Soil_pH,breaks=10) 
+hist(soils$Potassium,breaks=10) 
+boxplot(Soil_pH~Habitat,data=soils) 
+boxplot(Potassium~Habitat,data=soils) 
+
+##Log transform data 
+soils$log_K <- log(soils$Potassium)
+lm_log_K <- lm(log_K ~Habitat,data=soils) 
+anova(lm_log_K) 
+
+lm_log_K_resids <- resid(lm_K) 
+shapiro.test(lm_log_K_resids) 
+bartlett.test(log_K ~Habitat,data=soils) 
+plot(lm_log_K) 
